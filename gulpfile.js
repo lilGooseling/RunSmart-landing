afbeffe90b6ca85cc9d94fb5c67f246f14e0e7d9
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-03-14 22:31:20 +0300

    Header часть 2

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,14 @@ const cleanCSS = require('gulp-clean-css');
 // Static server
 gulp.task('server', function() {
     browserSync.init({
-        server: true
+        server: {
+            baseDir: "./"
+        }
     });
 });
 
 gulp.task('styles', function () {
-    return gulp.src("src/sass/*/*.+(scss|sass)")
+    return gulp.src("src/sass/blocks/style.scss")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
             prefix: "",
@@ -28,7 +30,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('watch', function () {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/sass/*/*.scss", gulp.parallel("styles")).on('change', browserSync.reload);
     gulp.watch("index.html").on("change", browserSync.reload);
 });
 
