4157e47feccbc2de67b395bbbddb12326b45648d
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-03-22 23:18:47 +0300

    carousel часть 2 + slides

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
