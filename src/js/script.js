5cd41305c4013addf2591d2741a74fe72bb5140d
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-03-24 22:27:09 +0300

    carousel adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,30 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="src/icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/arrow_right.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/arrow_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+
+        ]
     });
 });
