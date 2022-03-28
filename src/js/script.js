69ed9154fff7978d2e906bf8661b421b73066f15
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-03-28 22:05:51 +0300

    catalog items

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,17 @@ $(document).ready(function(){
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this)
             .index()).addClass('catalog__content_active');
     });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
