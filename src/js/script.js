390562aa00685f412661d392a32b58d7264a9ba6
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-04-02 22:38:17 +0300

    Modal

--- src/js/script.js
+++ src/js/script.js
@@ -48,4 +48,16 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //modal
+
+    $('[data-modal=consulting]').on('click', function () {
+        $('.overlay, #consulting').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consulting, #order, #thanks').fadeOut('slow');
+    });
+    $('.button_mini').on('click', function () {
+        $('.overlay, #order').fadeIn('slow');
+    });
 });
