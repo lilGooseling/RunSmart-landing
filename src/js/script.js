c4839a1464753999ce15bddef2752f2b957d79ca
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-04-07 22:14:52 +0300

    Отправка писем с сайта, часть 1

--- src/js/script.js
+++ src/js/script.js
@@ -100,4 +100,24 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+    $('form').submit(function(e) {
+        console.log(123);
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "http://runsmart.ru/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
 });
