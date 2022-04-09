2b23eb44c7f9a400f40696df383de1abe5a0390a
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-04-09 22:15:52 +0300

    Smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
         ]
     });
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this)
@@ -37,8 +37,8 @@ $(document).ready(function(){
     });
 
     function toggleSlide(item) {
-        $(item).each(function(i) {
-            $(this).on('click', function(e) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -67,7 +67,7 @@ $(document).ready(function(){
         });
     });
 
-    function valideForms (form) {
+    function valideForms(form) {
         $(form).validate({
             rules: {
                 name: {
@@ -100,7 +100,7 @@ $(document).ready(function(){
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-    $('form').submit(function(e) {
+    $('form').submit(function (e) {
         console.log(123);
         e.preventDefault();
 
@@ -112,7 +112,7 @@ $(document).ready(function(){
             type: "POST",
             url: "http://runsmart.ru/smart.php",
             data: $(this).serialize()
-        }).done(function() {
+        }).done(function () {
             $(this).find("input").val("");
             $('#consulting, #order').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
@@ -120,5 +120,19 @@ $(document).ready(function(){
         });
         return false;
     });
+    //Smooth scroll and pageup
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
